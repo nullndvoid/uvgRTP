29e7a2182e6545bcf2caf415c0089f52cc57416e
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-03-08 03:03:27 +0000

    Implement some examples in Zig
    TODO: Write a Zig wrapper

--- examples/recieve.zig
+++ examples/recieve.zig
@@ -1,9 +1,63 @@
 const std = @import("std");
 const c = @cImport(@cInclude("uvgrtp/wrapper_c.h"));
 
+const REMOTE_PORT = 8891;
+const LOCAL_PORT = 8890;
+const REMOTE_ADDRESS = "127.0.0.1";
+const N_TEST_PACKETS = 100;
+
 pub fn main() !void {
     const ctx = c.uvgrtp_create_ctx();
+
     if (ctx == null) {
         return error.InitialisationError;
+    }
+
+    defer c.uvgrtp_destroy_ctx(ctx);
+
+    const session = c.uvgrtp_create_session(ctx, REMOTE_ADDRESS);
+
+    if (session == null) {
+        return error.InitialisationError;
+    }
+
+    defer _ = c.uvgrtp_destroy_session(ctx, session);
+
+    const flags = c.RCE_RECEIVE_ONLY;
+
+    const opus_stream = c.uvgrtp_create_stream(
+        session,
+        LOCAL_PORT,
+        REMOTE_PORT,
+        c.RTP_FORMAT_OPUS,
+        flags,
+    );
+
+    if (opus_stream == null) {
+        return error.InitialisationError;
+    }
+
+    defer _ = c.uvgrtp_destroy_stream(session, opus_stream);
+
+    var recieved: u8 = 0;
+    while (recieved < N_TEST_PACKETS) {
+        const frame = c.uvgrtp_pull_frame(opus_stream);
+        defer c.uvgrtp_frame_free(frame);
+
+        if (frame == null) {
+            std.log.err(
+                "uvgrtp_pull_frame failed (rtp_errno={d})",
+                .{c.rtp_errno},
+            );
+
+            continue;
+        }
+
+        recieved += 1;
+
+        std.log.info(
+            "Recieved frame {d}/{d}, payload_len = {d}",
+            .{ recieved, N_TEST_PACKETS, frame.*.payload_len },
+        );
     }
 }
--- examples/sending.zig
+++ examples/sending.zig
@@ -1,1 +1,63 @@
-pub fn main() !void {}
+const std = @import("std");
+const c = @cImport(@cInclude("uvgrtp/wrapper_c.h"));
+
+const REMOTE_PORT = 8890;
+const LOCAL_PORT = 8891;
+const REMOTE_ADDRESS = "127.0.0.1";
+const N_TEST_PACKETS = 100;
+const PAYLOAD_LEN = 256;
+
+pub fn main(init: std.process.Init) !void {
+    const ctx = c.uvgrtp_create_ctx();
+
+    if (ctx == null) {
+        return error.InitialisationError;
+    }
+
+    defer c.uvgrtp_destroy_ctx(ctx);
+
+    const session = c.uvgrtp_create_session(ctx, REMOTE_ADDRESS);
+
+    if (session == null) {
+        return error.InitialisationError;
+    }
+
+    defer _ = c.uvgrtp_destroy_session(ctx, session);
+
+    const flags = c.RCE_SEND_ONLY;
+
+    const opus_stream = c.uvgrtp_create_stream(
+        session,
+        LOCAL_PORT,
+        REMOTE_PORT,
+        c.RTP_FORMAT_OPUS,
+        flags,
+    );
+
+    if (opus_stream == null) {
+        return error.InitialisationError;
+    }
+
+    defer _ = c.uvgrtp_destroy_stream(session, opus_stream);
+
+    const blank_frame = try init.arena.allocator().alloc(u8, PAYLOAD_LEN);
+    defer init.arena.allocator().free(blank_frame);
+
+    for (0..N_TEST_PACKETS) |i| {
+        @memset(blank_frame, 0x67);
+
+        if (c.uvgrtp_push_frame(
+            opus_stream,
+            blank_frame.ptr,
+            blank_frame.len,
+            c.RTP_NO_FLAGS,
+        ) != c.RTP_OK) {
+            std.log.err("Failed to send RTP frame {d} (rtp_errno={d})", .{ i, c.rtp_errno });
+            continue;
+        }
+
+        if ((i + 1) % 10 == 0 or i == 0) {
+            std.log.info("Sending frame {d}/{d}", .{ i + 1, N_TEST_PACKETS });
+        }
+    }
+}
